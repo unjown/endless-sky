2c30840705ad1ec3a934ed72a1ba67405ef5f813
quyykk <quyykk@protonmail.com>
2021-10-31 00:12:15 +0200

    Revert "Correctly fine illegal mission things."
    This reverts commit b00abe251c96571805a2b10a63bcb63501e957e4.

--- source/CargoHold.cpp
+++ source/CargoHold.cpp
@@ -578,7 +578,7 @@ int64_t CargoHold::IllegalCargoFine() const
 		if(fine < 0)
 			return fine;
 		if(!it.first->IsFailed())
-			totalFine += fine * it.second;
+			totalFine += fine;
 	}
 	
 	for(const auto &it : passengers)
@@ -587,7 +587,7 @@ int64_t CargoHold::IllegalCargoFine() const
 		if(fine < 0)
 			return fine;
 		if(!it.first->IsFailed())
-			totalFine += fine * it.second;
+			totalFine += fine;
 	}
 	
 	return totalFine;
